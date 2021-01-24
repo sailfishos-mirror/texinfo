9578b58ae53cb2b2b8c26380e10a9b63337ad729
Patrice Dumas <pertusus@free.fr>
2021-01-24 16:41:04 +0100

    Regenerate tex_html tests results

--- tp/tests/tex_html/res_parser/tex_gdef_l2h/tex_gdef-l2h_cache.pm
+++ tp/tests/tex_html/res_parser/tex_gdef_l2h/tex_gdef-l2h_cache.pm
@@ -4,25 +4,25 @@ $l2h_cache{$l2h_cache_key} = q|  |;
 
 $l2h_cache_key = q/$$ \Desig
 $$/;
-$l2h_cache{$l2h_cache_key} = q|<BR><P></P>
-<DIV CLASS="displaymath">
+$l2h_cache{$l2h_cache_key} = q|<DIV CLASS="displaymath">
 <!-- MATH
  \begin{displaymath}
 \buildrel \rm def \over \equiv
 \end{displaymath}
  -->
 
-<IMG STYLE="height: 3.12ex; vertical-align: -0.12ex; " SRC="tex_gdef_2.svg"
+<IMG
+ STYLE="height: 3.12ex; vertical-align: -0.12ex; " SRC="tex_gdef_2.svg"
  ALT="$\displaystyle \buildrel \rm def \over \equiv
 $">
 </DIV>
-<BR CLEAR="ALL">
-<P></P>|;
+|;
 
 $l2h_cache_key = q/$\Desig$/;
 $l2h_cache{$l2h_cache_key} = q|<!-- MATH
  $\buildrel \rm def \over \equiv$
  -->
-<SPAN CLASS="MATH"><IMG STYLE="height: 3.12ex; vertical-align: -0.12ex; " SRC="tex_gdef_1.svg"
+<SPAN CLASS="MATH"><IMG
+ STYLE="height: 3.12ex; vertical-align: -0.12ex; " SRC="tex_gdef_1.svg"
  ALT="$\buildrel \rm def \over \equiv $"></SPAN>|;
 1;
