25c679882d796f34b122366b37158faafd041442
Patrice Dumas <pertusus@free.fr>
2021-08-11 23:38:07 +0200

    Add coverage tests, mostly @-commands in @displaymath.

--- tp/tests/tex_html/res_parser/formatting_singular/sing-l2h_cache.pm
+++ tp/tests/tex_html/res_parser/formatting_singular/sing-l2h_cache.pm
@@ -1,4 +1,107 @@
 
+$l2h_cache_key = q/$$@"u @"{U} @~n @^a @'e @=o @`i @'{e} @dotless{i} @dotless{j} @`{@=E}
+@l{} @,{@'C} @,{@'C} @,c @H{a} @dotaccent{a} @ringaccent{a} @tieaccent{a}
+@u{a} @ubaraccent{a} @udotaccent{a} @v{a} @ogonek{a} a@sup{h}@sub{l}
+@* @ @  @
+@- @| @: @! @? @. @@ @} @{ @\/
+@today{}
+$$/;
+$l2h_cache{$l2h_cache_key} = q|<DIV CLASS="displaymath">
+<!-- MATH
+ \begin{displaymath}
+@"u @"{U} @~n @^a @'e @=o @`i @'{e} @dotless{i} @dotless{j} @`{@=E}
+@l{} @,{@'C} @,{@'C} @,c @H{a} @dotaccent{a} @ringaccent{a} @tieaccent{a}
+@u{a} @ubaraccent{a} @udotaccent{a} @v{a} @ogonek{a} a@sup{h}@sub{l}
+@* @ @  @
+@- @\| @: @! @? @. @@ @{ @} @/
+@today{}
+\end{displaymath}
+ -->
+
+<IMG
+ STYLE="height: 2.67ex; vertical-align: -0.70ex; " SRC="sing_5.svg"
+ ALT="$\displaystyle @''u @''{U} @~n @^a @'e @=o @\lq i @'{e} @dotless{i} @dotless{j} @\lq {...
+...@ogonek{a} a@sup{h}@sub{l}
+@* @ @ @
+@- @\vert @: @! @? @. @@ @{ @} @/
+@today{}
+$">
+</DIV>
+|;
+
+$l2h_cache_key = q/$$@click{}
+@U{0075}
+@TeX{} @LaTeX{} @bullet{} @copyright{} @dots{} @enddots{} @equiv{}
+@error{} @expansion{} @minus{} @point{} @print{} @result{}
+@aa{} @AA{} @ae{} @oe{} @AE{} @OE{} @o{} @O{} @ss{} @l{} @L{} @DH{}
+@TH{} @dh{} @th{} @exclamdown{} @questiondown{} @pounds{}
+@registeredsymbol{} @ordf{} @ordm{} @comma{} 
+$$/;
+$l2h_cache{$l2h_cache_key} = q|<DIV CLASS="displaymath">
+<!-- MATH
+ \begin{displaymath}
+@click{}
+@U{0075}
+@TeX{} @LaTeX{} @bullet{} @copyright{} @dots{} @enddots{} @equiv{}
+@error{} @expansion{} @minus{} @point{} @print{} @result{}
+@aa{} @AA{} @ae{} @oe{} @AE{} @OE{} @o{} @O{} @ss{} @l{} @L{} @DH{}
+@TH{} @dh{} @th{} @exclamdown{} @questiondown{} @pounds{}
+@registeredsymbol{} @ordf{} @ordm{} @comma{}
+\end{displaymath}
+ -->
+
+<IMG
+ STYLE="height: 2.29ex; vertical-align: -0.57ex; " SRC="sing_6.svg"
+ ALT="$\displaystyle @click{}
+@U{0075}
+@TeX{} @LaTeX{} @bullet{} @copyright{} @dots{} ...
+...down{} @questiondown{} @pounds{}
+@registeredsymbol{} @ordf{} @ordm{} @comma{}
+$">
+</DIV>
+|;
+
+$l2h_cache_key = q/$$@quotedblleft{} @quotedblright{} 
+@quoteleft{} @quoteright{} @quotedblbase{} @quotesinglbase{} @guillemetleft{}
+@guillemetright{} @guillemotleft{} @guillemotright{} @guilsinglleft{}
+@guilsinglright{} @textdegree{} @euro{} @arrow{} @leq{} @geq{}
+$$/;
+$l2h_cache{$l2h_cache_key} = q|<DIV CLASS="displaymath">
+<!-- MATH
+ \begin{displaymath}
+@quotedblleft{} @quotedblright{}
+@quoteleft{} @quoteright{} @quotedblbase{} @quotesinglbase{} @guillemetleft{}
+@guillemetright{} @guillemotleft{} @guillemotright{} @guilsinglleft{}
+@guilsinglright{} @textdegree{} @euro{} @arrow{} @leq{} @geq{}
+\end{displaymath}
+ -->
+
+<IMG
+ STYLE="height: 2.29ex; vertical-align: -0.57ex; " SRC="sing_7.svg"
+ ALT="$\displaystyle @quotedblleft{} @quotedblright{}
+@quoteleft{} @quoteright{} @quo...
+...guilsinglleft{}
+@guilsinglright{} @textdegree{} @euro{} @arrow{} @leq{} @geq{}
+$">
+</DIV>
+|;
+
+$l2h_cache_key = q/$$@strong{``simple-double--three---four----''} @w{aa}
+$$/;
+$l2h_cache{$l2h_cache_key} = q|<DIV CLASS="displaymath">
+<!-- MATH
+ \begin{displaymath}
+@strong{``simple-double- -three- - -four- - - -''} @w{aa}
+\end{displaymath}
+ -->
+
+<IMG
+ STYLE="height: 2.54ex; vertical-align: -0.57ex; " SRC="sing_4.svg"
+ ALT="$\displaystyle @strong{\lq\lq simple-double--three---four----''} @w{aa}
+$">
+</DIV>
+|;
+
 $l2h_cache_key = q/$$\partial_t \eta (t) = g(\eta(t),\varphi(t))$$ ''/;
 $l2h_cache{$l2h_cache_key} = q|<DIV CLASS="displaymath">
 <!-- MATH
@@ -25,7 +128,7 @@ f(x) = {1 \over \sigma \sqrt{2\pi}}e@sup{-{1 \over 2}\left({x-\mu \over \sigma}\
  -->
 
 <IMG
- STYLE="height: 6.26ex; vertical-align: -2.32ex; " SRC="sing_4.svg"
+ STYLE="height: 6.26ex; vertical-align: -2.32ex; " SRC="sing_8.svg"
  ALT="$\displaystyle disp--laymath
 f(x) = {1 \over \sigma \sqrt{2\pi}}e@sup{-{1 \over 2}\left({x-\mu \over \sigma}\right)^2}
 $">
