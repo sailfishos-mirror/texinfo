02b0a378aa18930497ecd35b41e0278c4bc431d4
Gavin Smith <gavinsmith0123@gmail.com>
2022-03-16 20:24:25 +0000

    2022 copyright year under info/

--- info/info.h
+++ info/info.h
@@ -1,6 +1,6 @@
 /* info.h -- Header file which includes all of the other headers.
 
-   Copyright 1993-2019 Free Software Foundation, Inc.
+   Copyright 1993-2022 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
