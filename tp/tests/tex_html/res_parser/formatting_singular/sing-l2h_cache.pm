e0a766888ffa123039d20ab0f17975f6265b365e
Patrice Dumas <pertusus@free.fr>
2022-07-25 01:25:37 +0200

    Improve protection of invalid Texinfo in coverage_macro.texi layout/formatting.texi

--- tp/tests/tex_html/res_parser/formatting_singular/sing-l2h_cache.pm
+++ tp/tests/tex_html/res_parser/formatting_singular/sing-l2h_cache.pm
@@ -1,80 +1,122 @@
 # Automatically generated
 use utf8;
 
-$l2h_cache_key = q/$$@"u @"{U} @~n @^a @'e @=o @`i @'{e} @dotless{i} @dotless{j} @`{@=E}
-@l{} @,{@'C} @,{@'C} @,c @H{a} @dotaccent{a} @ringaccent{a} @tieaccent{a}
-@u{a} @ubaraccent{a} @udotaccent{a} @v{a} @ogonek{a} a@sup{h}@sub{l}
-@* @ @  @
-@- @| @: @! @? @. @@ @} @{ @\/
-@today{}
+$l2h_cache_key = q/$$@"u @"{U} @~n @^a @'e @=o @`i @'{e} @`{@=E}
+@,{@'C} @,{@'C} @H{a} @dotaccent{a} @ringaccent{a} @tieaccent{a}
+@u{a} @v{a}
+@: @? @.
 $$/;
 $l2h_cache{$l2h_cache_key} = q|<DIV CLASS="displaymath">
 <!-- MATH
  \begin{displaymath}
-@"u @"{U} @~n @^a @'e @=o @`i @'{e} @dotless{i} @dotless{j} @`{@=E}
-@l{} @,{@'C} @,{@'C} @,c @H{a} @dotaccent{a} @ringaccent{a} @tieaccent{a}
-@u{a} @ubaraccent{a} @udotaccent{a} @v{a} @ogonek{a} a@sup{h}@sub{l}
-@* @ @  @
-@- @\| @: @! @? @. @@ @{ @} @/
-@today{}
+@"u @"{U} @~n @^a @'e @=o @`i @'{e} @`{@=E}
+@,{@'C} @,{@'C} @H{a} @dotaccent{a} @ringaccent{a} @tieaccent{a}
+@u{a} @v{a}
+@: @? @.
 \end{displaymath}
  -->
 
 <IMG
- STYLE="height: 2.67ex; vertical-align: -0.70ex; " SRC="sing_3.svg"
- ALT="$\displaystyle @''u @''{U} @~n @^a @'e @=o @\lq i @'{e} @dotless{i} @dotless{j} @\lq {...
-...@ogonek{a} a@sup{h}@sub{l}
-@* @ @ @
-@- @\vert @: @! @? @. @@ @{ @} @/
-@today{}
+ STYLE="height: 2.54ex; vertical-align: -0.57ex; " SRC="sing_10.svg"
+ ALT="$\displaystyle @''u @''{U} @~n @^a @'e @=o @\lq i @'{e} @\lq {@=E}
+@,{@'C} @,{@'C} @H{a} @dotaccent{a} @ringaccent{a} @tieaccent{a}
+@u{a} @v{a}
+@: @? @.
 $">
 </DIV>
 |;
 
-$l2h_cache_key = q/$$@b{b} @i{i} @r{r} @sc{sc} @t{t} @sansserif{sansserif} @slanted{slanted}
+$l2h_cache_key = q/$$@TeX{} @LaTeX{} @point{} @aa{} @registeredsymbol{} @textdegree{} 
 $$/;
 $l2h_cache{$l2h_cache_key} = q|<DIV CLASS="displaymath">
 <!-- MATH
  \begin{displaymath}
-@b{b} @i{i} @r{r} @sc{sc} @t{t} @sansserif{sansserif} @slanted{slanted}
+@TeX{} @LaTeX{} @point{} @aa{} @registeredsymbol{} @textdegree{}
 \end{displaymath}
  -->
 
 <IMG
- STYLE="height: 2.29ex; vertical-align: -0.57ex; " SRC="sing_6.svg"
- ALT="$\displaystyle @b{b} @i{i} @r{r} @sc{sc} @t{t} @sansserif{sansserif} @slanted{slanted}
+ STYLE="height: 2.29ex; vertical-align: -0.57ex; " SRC="sing_11.svg"
+ ALT="$\displaystyle @TeX{} @LaTeX{} @point{} @aa{} @registeredsymbol{} @textdegree{}
+$">
+</DIV>
+|;
+
+$l2h_cache_key = q/$$@b{b} @i{i} @r{r} @sc{sc} @sansserif{sansserif} @slanted{slanted}
+$$/;
+$l2h_cache{$l2h_cache_key} = q|<DIV CLASS="displaymath">
+<!-- MATH
+ \begin{displaymath}
+@b{b} @i{i} @r{r} @sc{sc} @sansserif{sansserif} @slanted{slanted}
+\end{displaymath}
+ -->
+
+<IMG
+ STYLE="height: 2.29ex; vertical-align: -0.57ex; " SRC="sing_7.svg"
+ ALT="$\displaystyle @b{b} @i{i} @r{r} @sc{sc} @sansserif{sansserif} @slanted{slanted}
 $">
 </DIV>
 |;
 
 $l2h_cache_key = q/$$@click{}
 @U{0075}
-@TeX{} @LaTeX{} @bullet{} @copyright{} @dots{} @enddots{} @equiv{}
-@error{} @expansion{} @minus{} @point{} @print{} @result{}
-@aa{} @AA{} @ae{} @oe{} @AE{} @OE{} @o{} @O{} @ss{} @l{} @L{} @DH{}
+@bullet{} @copyright{} @dots{} @enddots{} @equiv{}
+@error{} @expansion{} @minus{} @print{} @result{}
+@AA{} @ae{} @oe{} @AE{} @OE{} @o{} @O{} @ss{} @l{} @L{} @DH{}
 @TH{} @dh{} @th{} @exclamdown{} @questiondown{} @pounds{}
-@registeredsymbol{} @ordf{} @ordm{} @comma{} 
+@ordf{} @ordm{} @comma{} 
 $$/;
 $l2h_cache{$l2h_cache_key} = q|<DIV CLASS="displaymath">
 <!-- MATH
  \begin{displaymath}
 @click{}
 @U{0075}
-@TeX{} @LaTeX{} @bullet{} @copyright{} @dots{} @enddots{} @equiv{}
-@error{} @expansion{} @minus{} @point{} @print{} @result{}
-@aa{} @AA{} @ae{} @oe{} @AE{} @OE{} @o{} @O{} @ss{} @l{} @L{} @DH{}
+@bullet{} @copyright{} @dots{} @enddots{} @equiv{}
+@error{} @expansion{} @minus{} @print{} @result{}
+@AA{} @ae{} @oe{} @AE{} @OE{} @o{} @O{} @ss{} @l{} @L{} @DH{}
 @TH{} @dh{} @th{} @exclamdown{} @questiondown{} @pounds{}
-@registeredsymbol{} @ordf{} @ordm{} @comma{}
+@ordf{} @ordm{} @comma{}
 \end{displaymath}
  -->
 
 <IMG
- STYLE="height: 2.29ex; vertical-align: -0.57ex; " SRC="sing_4.svg"
+ STYLE="height: 2.29ex; vertical-align: -0.57ex; " SRC="sing_5.svg"
  ALT="$\displaystyle @click{}
 @U{0075}
-@TeX{} @LaTeX{} @bullet{} @copyright{} @dots{} ...
-...down{} @questiondown{} @pounds{}
-@registeredsymbol{} @ordf{} @ordm{} @comma{}
+@bullet{} @copyright{} @dots{} @enddots{} @equi...
+... @dh{} @th{} @exclamdown{} @questiondown{} @pounds{}
+@ordf{} @ordm{} @comma{}
+$">
+</DIV>
+|;
+
+$l2h_cache_key = q/$$@dotless{i} @dotless{j}
+@l{} @,c
+@ubaraccent{a} @udotaccent{a} @ogonek{a} a@sup{h}@sub{l}
+@* @ @  @
+@- @| @! @@ @} @{ @\/
+@today{}
+$$/;
+$l2h_cache{$l2h_cache_key} = q|<DIV CLASS="displaymath">
+<!-- MATH
+ \begin{displaymath}
+@dotless{i} @dotless{j}
+@l{} @,c
+@ubaraccent{a} @udotaccent{a} @ogonek{a} a@sup{h}@sub{l}
+@* @ @  @
+@- @\| @! @@ @{ @} @/
+@today{}
+\end{displaymath}
+ -->
+
+<IMG
+ STYLE="height: 2.55ex; vertical-align: -0.70ex; " SRC="sing_4.svg"
+ ALT="$\displaystyle @dotless{i} @dotless{j}
+@l{} @,c
+@ubaraccent{a} @udotaccent{a} @ogonek{a} a@sup{h}@sub{l}
+@* @ @ @
+@- @\vert @! @@ @{ @} @/
+@today{}
 $">
 </DIV>
 |;
@@ -82,7 +124,7 @@ $">
 $l2h_cache_key = q/$$@quotedblleft{} @quotedblright{} 
 @quoteleft{} @quoteright{} @quotedblbase{} @quotesinglbase{} @guillemetleft{}
 @guillemetright{} @guillemotleft{} @guillemotright{} @guilsinglleft{}
-@guilsinglright{} @textdegree{} @euro{} @arrow{} @leq{} @geq{}
+@guilsinglright{} @euro{} @arrow{} @leq{} @geq{}
 $$/;
 $l2h_cache{$l2h_cache_key} = q|<DIV CLASS="displaymath">
 <!-- MATH
@@ -90,16 +132,16 @@ $l2h_cache{$l2h_cache_key} = q|<DIV CLASS="displaymath">
 @quotedblleft{} @quotedblright{}
 @quoteleft{} @quoteright{} @quotedblbase{} @quotesinglbase{} @guillemetleft{}
 @guillemetright{} @guillemotleft{} @guillemotright{} @guilsinglleft{}
-@guilsinglright{} @textdegree{} @euro{} @arrow{} @leq{} @geq{}
+@guilsinglright{} @euro{} @arrow{} @leq{} @geq{}
 \end{displaymath}
  -->
 
 <IMG
- STYLE="height: 2.29ex; vertical-align: -0.57ex; " SRC="sing_5.svg"
+ STYLE="height: 2.29ex; vertical-align: -0.57ex; " SRC="sing_6.svg"
  ALT="$\displaystyle @quotedblleft{} @quotedblright{}
 @quoteleft{} @quoteright{} @quo...
-...guilsinglleft{}
-@guilsinglright{} @textdegree{} @euro{} @arrow{} @leq{} @geq{}
+...lemotright{} @guilsinglleft{}
+@guilsinglright{} @euro{} @arrow{} @leq{} @geq{}
 $">
 </DIV>
 |;
@@ -116,9 +158,25 @@ $l2h_cache{$l2h_cache_key} = q|<DIV CLASS="displaymath">
  -->
 
 <IMG
- STYLE="height: 2.54ex; vertical-align: -0.57ex; " SRC="sing_2.svg"
+ STYLE="height: 2.54ex; vertical-align: -0.57ex; " SRC="sing_3.svg"
  ALT="$\displaystyle @strong{\lq\lq simple-double--three---four----''} @w{aa}
 \lq @w{}\lq simple-double-@w{}-three---four----'@w{}'@*
+$">
+</DIV>
+|;
+
+$l2h_cache_key = q/$$@t{t} 
+$$/;
+$l2h_cache{$l2h_cache_key} = q|<DIV CLASS="displaymath">
+<!-- MATH
+ \begin{displaymath}
+@t{t}
+\end{displaymath}
+ -->
+
+<IMG
+ STYLE="height: 1.84ex; vertical-align: -0.12ex; " SRC="sing_12.svg"
+ ALT="$\displaystyle @t{t}
 $">
 </DIV>
 |;
@@ -149,7 +207,7 @@ f(x) = {1 \over \sigma \sqrt{2\pi}}e@sup{-{1 \over 2}\left({x-\mu \over \sigma}\
  -->
 
 <IMG
- STYLE="height: 6.26ex; vertical-align: -2.32ex; " SRC="sing_7.svg"
+ STYLE="height: 6.26ex; vertical-align: -2.32ex; " SRC="sing_2.svg"
  ALT="$\displaystyle disp--laymath
 f(x) = {1 \over \sigma \sqrt{2\pi}}e@sup{-{1 \over 2}\left({x-\mu \over \sigma}\right)^2}
 $">
