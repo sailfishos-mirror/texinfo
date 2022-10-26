ebf80ad9dbab767dc5d6c5284450ad37150c59f6
Patrice Dumas <pertusus@free.fr>
2022-10-26 17:46:35 +0200

    * tp/maintain/copy_change_file_name_encoding.pl: use a character appearing in more 8bit encodings in the test of 8bit encoded include file.  Eli Zaretskii idea.

--- tp/maintain/copy_change_file_name_encoding.pl
+++ tp/maintain/copy_change_file_name_encoding.pl
@@ -56,8 +56,9 @@ if (defined($dest_dir)) {
 
 my $converted_dest_path = Encode::decode($from, $dest_path);
 # not that converted_dest_path may not be in UTF-8, depends what perl internally
-# does
-$converted_dest_path =~ s/latin/latîn/;
+# does.
+# The Yen and Yuan sign is used as it is common to many 8bit codepages.
+$converted_dest_path =~ s/latin/lat¥n/;
 my $dest_path_in_utf8 = Encode::encode('UTF-8', $converted_dest_path);
 # use another variable, since from_to argument is converted in-place
 my $dest_path_in_to_encoding = $dest_path_in_utf8;
