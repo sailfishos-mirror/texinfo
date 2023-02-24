sailfishos-mirror/texinfo

commit 93162b78d84f731710025b300bc0cd7ca8b14347

Atomic dir update
* install-info/install-info.c (output_dirfile): Write to
file created by mkstemp first, then rename this to the real
output file.
* TODO: Remove item.

Report from Pedrum Mohageri, bug-texinfo, November 2015.

install-info/install-info.c
@@ -923,14 +923,19 @@ output_dirfile (char *dirfile, int dir_nlines, struct line_data *dir_lines,
   int n_entries_added = 0;
   int i;
   FILE *output;
+  int tempfile; /* moved to dirfile when finished */
+  char tempname[] = "infodirXXXXXX";
 
+  tempfile = mkstemp (tempname);
   if (compression_program)
     {
-      char *command = concat (compression_program, ">", dirfile);
+      char *command;
+      close (tempfile);
+      command = concat (compression_program, ">", tempname);
       output = popen (command, "w");
     }
   else
-    output = fopen (dirfile, "w");
+    output = fdopen (tempfile, "w");
 
   if (!output)
     {
@@ -1045,6 +1050,11 @@ output_dirfile (char *dirfile, int dir_nlines, struct line_data *dir_lines,
     pclose (output);
   else
     fclose (output);
+
+  /* Update dir file atomically.  This stops the dir file being corrupted
+     if install-info is interrupted. */
+  if (rename (tempname, dirfile) == -1)
+    perror (tempname);
 }
 
 /* Read through the input LINES, to find the section names and the

TODO
@@ -91,9 +91,6 @@ If you are interested in working on any of these, email bug-texinfo@gnu.org.
   - Handle M-n, C-u m, and C-u g like Emacs Info (opening new windows).
 
 * install-info:
-  - replace dir file atomically in case install-info is interrupted
-    (Pedrum Mohageri,
-    https://lists.gnu.org/archive/html/bug-texinfo/2015-11/msg00027.html)
   - install-info --dir-file=dir2 --entry=info foo.info installs garbage
     from the misformatted --entry (and no entries from foo.info).
   - support info dir entries for other than the current info file name;

commit 94ac7b4ecc69b46b3ecbc9591ace0f1a1c39836b

install-info: Fix dir file mode being restricted
The recent switch to mkstemp and atomic updating of the directory
file unintentionally lead to overly restrictive permissions being
set on directory file.  This error was originally introduced in
93162b78d84f731710025b300bc0cd7ca8b14347 (2022-12-03 Gavin Smith
<gavinsmith0123@gmail.com>).
* install-info/install-info.c (output_dirfile): Change mode of
installed dir file back to its usual value, rather than the
mkstemp default of 0600.

install-info/install-info.c
@@ -1053,6 +1053,16 @@ output_dirfile (char *dirfile, int dir_nlines, struct line_data *dir_lines,
   else
     fclose (output);
 
+  /* Reset the mode that the file is set to.  */
+  mode_t um = umask (0022);
+  umask (um);
+  if (fchmod (tempfile, 0666 & um) < 0)
+    {
+      perror ("chmod");
+      remove (tempname);
+      exit (EXIT_FAILURE);
+    }
+
   /* Update dir file atomically.  This stops the dir file being corrupted
      if install-info is interrupted. */
   if (rename (tempname, dirfile) == -1)