1d57bbbd866a4c7ea0b7dfbfa7688f6d6fa24687
Gavin Smith <gavinsmith0123@gmail.com>
2023-08-04 09:55:35 +0100

    Revert commit afca9f95f8dcca047ad9.
    This reverts the following change:
    
    2023-07-28  Patrice Dumas  <pertusus@free.fr>
    
      * tp/texi2any.pl (set_translations_encoding): reset translations
      encoding when COMMAND_LINE_ENCODING is reset.
      * tp/texi2any.pl: move code around for a more logical organization.
    
    Werner Lemberg reported incorrectly encoded output for
    "makeinfo --help" for LANG=en_US.UTF-8 LANGUAGE=de and tracked it
    down to this commit.

--- tp/Texinfo/Config.pm
+++ tp/Texinfo/Config.pm
@@ -121,7 +121,6 @@ sub _GNUT_document_warn($) {
                    "%s: warning: %s"), $real_command_name, $text)."\n"));
 }
 
-# used to register messages by the user with texinfo_register_init_loading_*
 my @init_file_loading_messages;
 # called from texi2any.pl main program and t/test_utils.pl.
 # eval $FILE in the Texinfo::Config namespace. $FILE should be a binary string.
@@ -171,7 +170,7 @@ sub texinfo_register_init_loading_error($) {
                                             'text' => $message};
 }
 
-# called from init files in case of warnings at loading.
+# called from init files in case for warnings during loading.
 sub texinfo_register_init_loading_warning($) {
   my $message = shift;
   push @{$init_file_loading_messages[-1]}, {'type' => 'warning',
@@ -354,7 +353,7 @@ my @possible_stages = ('setup', 'structure', 'init', 'finish');
 
 my $default_priority = 'default';
 
-# TODO add another level with format?  Not needed now as HTML is
+# FIXME add another level with format?  Not needed now as HTML is
 # the only customizable format for now.
 my $GNUT_stage_handlers;
 
