00c112952e906477eb3b3a5dd1d4fa2a90307867
Gavin Smith <gavinsmith0123@gmail.com>
2023-08-15 10:32:58 +0100

    more copyright years

--- Pod-Simple-Texinfo/lib/Pod/Simple/Texinfo.pm
+++ Pod-Simple-Texinfo/lib/Pod/Simple/Texinfo.pm
@@ -1,6 +1,6 @@
 # Texinfo.pm: format Pod as Texinfo.
 #
-# Copyright 2011, 2012, 2013, 2014, 2022 Free Software Foundation, Inc.
+# Copyright 2011-2023 Free Software Foundation, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
