921d119e630ef68243391aadf8c0cfcaadf63b1d
Patrice Dumas <pertusus@free.fr>
2023-09-22 09:16:09 +0200

    * tp/Texinfo/Common.pm ($XS_only), tp/Texinfo/Structuring.pm ($XS_only), tp/Texinfo/Transformations.pm ($XS_only): set $XS_only if both TEXINFO_XS is set to require and TEXINFO_XS_CONVERT is set.

--- tp/Texinfo/Transformations.pm
+++ tp/Texinfo/Transformations.pm
@@ -98,7 +98,9 @@ sub import {
 # set to 1 if perl code is to be run only if XS is not set
 my $XS_only = 0;
 $XS_only = 1 if (defined($ENV{'TEXINFO_XS'})
-                 and $ENV{'TEXINFO_XS'} eq 'require');
+                 and $ENV{'TEXINFO_XS'} eq 'require'
+                 and defined($ENV{'TEXINFO_XS_CONVERT'})
+                 and $ENV{'TEXINFO_XS_CONVERT'});
 
 
 # Add raise/lowersections to be back at the normal level from
