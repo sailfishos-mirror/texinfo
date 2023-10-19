bc7b85b5d5d4699e37b429fc1e199c16d78ade31
Gavin Smith <gavinsmith0123@gmail.com>
2023-10-19 12:50:33 +0100

    * tp/maintain/change_perl_modules_version.sh: Also update *.pm.in in order to updae Texinfo/ModulePath.pm.in, which was stuck at "6.3dev".  Remove commented-out sed version. Add usage message in case not invoked properly.

--- tp/maintain/change_perl_modules_version.sh
+++ tp/maintain/change_perl_modules_version.sh
@@ -1,14 +1,20 @@
 #! /bin/sh
 
+if test z"$1" = "z" ; then
+  echo "usage: $0 auto"
+  exit 1
+fi
+
 VERS=$1
 if test z"$VERS" = "zauto" ; then 
   VERS=`grep '^AC_INIT' ../configure.ac | sed -e 's/^[^0-9]*//' -e 's/].*//'`
   echo vers is $VERS
-fi
-if test z"$VERS" = "z" ; then
-  exit 1
+  if test z"$VERS" = "z" ; then
+    echo "cannot proceed"
+    exit 1
+  fi
 fi
  
 
-#find Texinfo/ -name '*.pm' | xargs sed -i -e 's/\$VERSION = '"'"'\([0-9.]\+\)'"'"'/$VERSION = '"'$VERS'"'/'
-find Texinfo/ -name '*.pm' | xargs perl -pi -e 's/\$VERSION = '"'([0-9.]+[^']*)'"'/\$VERSION = '"'$VERS'"'/'
+find Texinfo/ -name '*.pm' -o -name '*.pm.in' | xargs \
+  perl -pi -e 's/\$VERSION = '"'([0-9.]+[^']*)'"'/\$VERSION = '"'$VERS'"'/'
