8d448f230d51ed11df04a73912964b4bcb7eaf75
Patrice Dumas <pertusus@free.fr>
2024-04-17 00:51:23 +0200

    * Pod-Simple-Texinfo/Makefile.am (test_files, EXTRA_DIST), Pod-Simple-Texinfo/test_scripts/*.sh: add tests of pod2texi.pl, based on tests formerly in manual_tests.

--- Pod-Simple-Texinfo/manual_tests/test_empty_head2.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-# From the Pod-Simple-Texinfo, run
-#  ./manual_tests/test_empty_head2.sh
-
-# The manual processed in the test has an empty head.
-# There are errors generated when parsing the manual to fix and add
-# menus, which can be seen with --debug=2.
-./pod2texi.pl manual_tests/empty_head2.pod | diff -u manual_tests/reference_test_empty_head2.texi -
--- Pod-Simple-Texinfo/manual_tests/test_ref_to_unknown.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-# From the Pod-Simple-Texinfo, run
-#  ./manual_tests/test_ref_to_unknown.sh
-
-# The manual processed in the test has a reference that is not resolved.
-# There is an error generated when gathering internal references after
-# parsing the manual to fix and add  menus, which can be seen with --debug=2.
-./pod2texi.pl manual_tests/ref_to_unknown.pod | diff -u manual_tests/reference_ref_to_unknown.texi -
