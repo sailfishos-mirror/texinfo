02d32938d946272e911622ae29640590c763b075
Patrice Dumas <pertusus@free.fr>
2024-04-16 23:46:13 +0200

    Add Pod-Simple-Texinfo manual test.

--- Pod-Simple-Texinfo/manual_tests/test_ref_to_unknown.sh
+++ Pod-Simple-Texinfo/manual_tests/test_ref_to_unknown.sh
@@ -4,6 +4,6 @@
 #  ./manual_tests/test_ref_to_unknown.sh
 
 # The manual processed in the test has a reference that is not resolved.
-# There is an error generated when parsing the manual to fix and add
-# menus, which is, normally, not visible.
+# There is an error generated when gathering internal references after
+# parsing the manual to fix and add  menus, which can be seen with --debug=2.
 ./pod2texi.pl manual_tests/ref_to_unknown.pod | diff -u manual_tests/reference_ref_to_unknown.texi -
