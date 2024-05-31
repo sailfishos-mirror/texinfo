da28c6d21ef1d3c8d193891e8127bccdefbe0bc8
Patrice Dumas <pertusus@free.fr>
2024-05-31 23:59:21 +0200

    Add data structure for information on types in C
    * tp/Texinfo/XS/main/types_data.h (TYPES), tp/Texinfo/XS/Makefile.am
    (BUILT_SOURCES, libtexinfo_la_SOURCES, EXTRA_DIST),
    tp/Texinfo/XS/main/element_types.awk,
    tp/Texinfo/XS/main/element_types.txt: add code in element_types.awk to
    setup a TYPE structure for information on types, similar to the
    COMMAND data, but with flags only.  Add flags information in
    element_types.txt, to mark text elements.  Generate the structure
    array with information on all the types in main/types_data.c.

---
+++ tp/Texinfo/XS/main/types_data.h
@@ -0,0 +1,30 @@
+/* types_data.h - declarations for types_data.c */
+#ifndef TYPES_DATA_H
+#define TYPES_DATA_H
+/* Copyright 2010-2024 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+#include "element_types.h"
+
+typedef struct types_struct {
+  char *type_name;
+  unsigned long flags;
+} TYPE;
+
+extern TYPE type_data[];
+
+#define TF_text               0x0001
+
+#endif
