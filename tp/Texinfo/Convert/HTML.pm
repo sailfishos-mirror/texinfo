89fdbb8313307712f275c1cc766c082ed6750e1f
Patrice Dumas <pertusus@free.fr>
2024-07-09 11:39:45 +0200

    * tp/Texinfo/Convert/HTML.pm (%default_no_arg_commands_formatting): for css_string preset explicitly @tie and @* values, and use nobrace_symbol_text preferentially to preformatted/normal values.

--- tp/Texinfo/Convert/HTML.pm
+++ tp/Texinfo/Convert/HTML.pm
@@ -3283,19 +3283,18 @@ $default_no_arg_commands_formatting{'preformatted'}->{'*'} = {'text' => "\n"};
 # is protected as CSS as a\'b", and " is escaped in an HTML style
 # attribute: style="list-style-type: 'a\'b&quot;'"
 
-# Currently no need, the commands in nobrace_symbol_text are mainly
-# taken from normal formatting, in turn mainly from
-# xml_text_entity_no_arg_commands_formatting, which copies
-# nobrace_symbol_text except for &, and spaces commands that are explicitly
-# set to nbsp above are explicitly reset to spaces.
-# FIXME use nobrace_symbol_text instead?
-#foreach my $no_brace_command (keys(%nobrace_symbol_text)) {
-#  $default_no_arg_commands_formatting{'css_string'}->{$no_brace_command}
-#   = {'text' => $nobrace_symbol_text{$no_brace_command}};
-#}
+# for the commands without a good representation in the other maps
+my %css_no_arg_commands = (
+  # not in unicode maps and we want to avoid &nbsp; from other possibilities
+  'tie' => ' ',
+  '*' => '\A ',
+);
 
 foreach my $command (keys(%{$default_no_arg_commands_formatting{'normal'}})) {
-  if (defined($Texinfo::Convert::Unicode::unicode_map{$command})
+  if (exists($css_no_arg_commands{$command})) {
+    $default_no_arg_commands_formatting{'css_string'}->{$command}
+      = {'text' => $css_no_arg_commands{$command}};
+  } elsif (defined($Texinfo::Convert::Unicode::unicode_map{$command})
       and $Texinfo::Convert::Unicode::unicode_map{$command} ne '') {
     my $char_nr = hex($Texinfo::Convert::Unicode::unicode_map{$command});
     my $css_string;
@@ -3306,19 +3305,19 @@ foreach my $command (keys(%{$default_no_arg_commands_formatting{'normal'}})) {
     }
     $default_no_arg_commands_formatting{'css_string'}->{$command}
        = {'text' => $css_string};
+  } elsif (exists($nobrace_symbol_text{$command})) {
+    $default_no_arg_commands_formatting{'css_string'}->{$command}
+      = {'text' => $nobrace_symbol_text{$command}};
   } elsif ($default_no_arg_commands_formatting{'preformatted'}->{$command}) {
-    # avoid text with HTML element for @* @enddots
+    # avoid text with HTML element.  For @enddots
     $default_no_arg_commands_formatting{'css_string'}->{$command}
      = {'text'
         => $default_no_arg_commands_formatting{'preformatted'}->{$command}->{'text'}};
   } elsif ($default_no_arg_commands_formatting{'normal'}->{$command}->{'text'}) {
+    # complete the commands not in unicode maps: TeX, error, LaTeX
+    # (and tie would have been used too if it was not explicitly set).
     $default_no_arg_commands_formatting{'css_string'}->{$command}
      = {'text' => $default_no_arg_commands_formatting{'normal'}->{$command}->{'text'}};
-  } elsif (exists($nobrace_symbol_text{$command})
-           and $nobrace_symbol_text{$command} eq '') {
-    # @- @: @/ @|
-    $default_no_arg_commands_formatting{'css_string'}->{$command}
-                                                    = {'text' => ''};
   } else {
     warn "BUG: $command: no css_string\n";
   }
@@ -3328,14 +3327,6 @@ foreach my $command (keys(%{$default_no_arg_commands_formatting{'normal'}})) {
 # is in the default converter translated commands)
 delete $default_no_arg_commands_formatting{'css_string'}->{'error'};
 
-$default_no_arg_commands_formatting{'css_string'}->{'*'}->{'text'} = '\A ';
-
-$default_no_arg_commands_formatting{'css_string'}->{' '}->{'text'} = ' ';
-$default_no_arg_commands_formatting{'css_string'}->{"\t"}->{'text'} = ' ';
-$default_no_arg_commands_formatting{'css_string'}->{"\n"}->{'text'} = ' ';
-$default_no_arg_commands_formatting{'css_string'}->{'tie'}->{'text'} = ' ';
-
-$default_no_arg_commands_formatting{'css_string'}->{'&'}->{'text'} = '&';
 
 # w not in css_string, set the corresponding css_element_class_styles
 # especially, which also has none and not w in the class
