e98aaea566cc03347775534225347f655c0fa020
Patrice Dumas <pertusus@free.fr>
2024-09-29 15:49:57 +0200

    Update test result

--- tp/t/results/coverage_braces/seeentry_no_braces.pl
+++ tp/t/results/coverage_braces/seeentry_no_braces.pl
@@ -399,7 +399,7 @@ $result_converted{'html_text'}->{'seeentry_no_braces'} = '<div class="top-level-
 <h2 class="chapter" id="chap-1"><span>1 chap<a class="copiable-link" href="#chap-1"> &para;</a></span></h2>
 
 <div class="printindex cp-printindex">
-<table class="cp-entries-printindex" border="0">
+<table class="cp-entries-printindex">
 <tr><td></td><th class="entries-header-printindex">Index Entry</th><th class="sections-header-printindex">Section</th></tr>
 <tr><td colspan="3"><hr></td></tr>
 <tr><th id="chap_cp_letter-A">A</th></tr>
