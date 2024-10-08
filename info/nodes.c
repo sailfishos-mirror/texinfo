e8717b6aed4340f85e35f91983c6672bedd4848e
Patrice Dumas <pertusus@free.fr>
2024-10-08 08:51:39 +0200

    * info/nodes.c (get_tags_of_indirect_tags_table): ignore a bogus entry in indirect tags table with a negative offset for a file.

--- info/nodes.c
+++ info/nodes.c
@@ -385,17 +385,24 @@ get_tags_of_indirect_tags_table (FILE_BUFFER *file_buffer,
   while (line < end)
     {
       int colon;
+      long first_byte;
 
       colon = string_in_line (":", line);
 
       if (colon == -1)
         break;
 
+      first_byte = atol (line + colon);
+
+      /* ignore an entry with a bogus negative offset */
+      if (first_byte < 0)
+        continue;
+
       subfile = xmalloc (sizeof (SUBFILE));
       subfile->filename = xmalloc (colon);
       strncpy (subfile->filename, line, colon - 1);
       subfile->filename[colon - 1] = 0;
-      subfile->first_byte = (long) atol (line + colon);
+      subfile->first_byte = first_byte;
 
       add_pointer_to_array (subfile, subfiles_index, subfiles, 
                             subfiles_slots, 10);
