0ac3d7528d0c38350c7e4770c31a4d282f1b120a
Gavin Smith <gavinsmith0123@gmail.com>
2024-10-15 22:04:28 +0100

    Fix install-info memory leak
    * install-info/install-info.c (open_possibly_compressed_file):
    Move a function call returning memory that has to be freed
    to prevent a theoretical memory leak in case of an error calling
    fclose or freopen.  Report from Vitezslav Crhonek.

--- install-info/install-info.c
+++ install-info/install-info.c
@@ -825,12 +825,12 @@ determine_file_type:
     {
       /* Redirect stdin to the file and fork the decompression process
          reading from stdin.  This allows shell metacharacters in filenames. */
-      char *command = concat (*compression_program, " -d", "");
 
       if (fclose (f) < 0)
         return 0;
       if (!freopen (*opened_filename, FOPEN_RBIN, stdin))
         return 0;
+      char *command = concat (*compression_program, " -d", "");
       f = popen (command, "r");
       fclose (stdin);
       if (!f)
