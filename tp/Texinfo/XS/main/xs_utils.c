4c78d65ee740fac4f4cac43b595cf1dd6a0c8b34
Patrice Dumas <pertusus@free.fr>
2024-11-15 13:56:23 +0100

    * tp/Texinfo/XS/Makefile.am (libxsutils_la_CPPFLAGS) (libxsutils_la_LIBADD, libtexinfo_la_LIBADD), tp/Texinfo/XS/main/xs_utils.c: include config.h in xs_utils.c as vasprintf may come from Gnulib.  Add Gnulib flags and library for libxsutils.la and remove from libtexinfo.la to avoid doubly defined symbols.

--- tp/Texinfo/XS/main/xs_utils.c
+++ tp/Texinfo/XS/main/xs_utils.c
@@ -13,6 +13,9 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
+/* needed for vasprintf which may come from Gnulib */
+#include <config.h>
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
