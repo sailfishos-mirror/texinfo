bec77db7e14f6fdb41014b9c84bf17b554c0e553
Gavin Smith <gavinsmith0123@gmail.com>
2024-12-23 14:51:13 +0000

    Remove system.h defines
    * system.h [STAT_MACROS_BROKEN]: Remove section as this symbol
    is not set anywhere.  It was formerly set by AC_HEADER_STAT in
    configure.ac but this macro is now obsolescent..

--- system.h
+++ system.h
@@ -66,9 +66,6 @@ extern int errno;
 #endif
 
 #include <sys/stat.h>
-#if STAT_MACROS_BROKEN
-# undef S_ISDIR
-#endif
 #if !defined(S_ISDIR) && defined(S_IFDIR)
 # define S_ISDIR(mode) (((mode) & S_IFMT) == S_IFDIR)
 #endif
