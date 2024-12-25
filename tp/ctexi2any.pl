53e20cb01c49e2130b0f743da4b69f776a90d8b4
Patrice Dumas <pertusus@free.fr>
2024-12-25 11:20:17 +0100

    * tp/ctexi2any.pl: add a ctexi2any wrapper that can be called as a Perl script, to be called for tests.

---
+++ tp/ctexi2any.pl
@@ -0,0 +1,18 @@
+#! /usr/bin/env perl
+
+use strict;
+use File::Basename;
+
+
+my $command_dir;
+my $top_srcdir = $ENV{'top_srcdir'};
+if (defined($top_srcdir)) {
+  $command_dir = "$top_srcdir/tp";
+} else {
+  my ($real_command_name, $command_directory, $command_suffix)
+    = fileparse($0, '.pl');
+  $command_dir = $command_directory;
+  $ENV{'top_srcdir'} = "$command_dir/..";
+}
+exec("${command_dir}/Texinfo/XS/ctexi2any", @ARGV);
+
