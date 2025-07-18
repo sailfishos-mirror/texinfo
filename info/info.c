ce1d31676cac8cf1e0d122c89c6f40af44abfdd2
Gavin Smith <gavinsmith0123@gmail.com>
2025-07-18 22:15:19 +0100

    * info/info.c (main): Reformat string constant in usage message so to have whitespace in first column.  No functional change.

--- info/info.c
+++ info/info.c
@@ -923,10 +923,11 @@ main (int argc, char *argv[])
     {
       printf ("info (GNU %s) %s\n", PACKAGE, VERSION);
       puts ("");
-      printf (_("Copyright (C) %s Free Software Foundation, Inc.\n\
-License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\
-This is free software: you are free to change and redistribute it.\n\
-There is NO WARRANTY, to the extent permitted by law.\n"),
+      printf (_("Copyright (C) %s Free Software Foundation, Inc.\n"
+  "License GPLv3+: GNU GPL version 3 or later"
+  " <http://gnu.org/licenses/gpl.html>\n"
+  "This is free software: you are free to change and redistribute it.\n"
+  "There is NO WARRANTY, to the extent permitted by law.\n"),
 	      "2025");
       exit (EXIT_SUCCESS);
     }
