924685c2fc437a34c1a80f74d037f3b1fd4d9e61
Patrice Dumas <pertusus@free.fr>
2025-11-01 19:30:16 +0100

    Regenerate test results for a test skipped with XS

--- tta/perl/t/results/html_tests/spaces_in_line_break_in_verb_w.pl
+++ tta/perl/t/results/html_tests/spaces_in_line_break_in_verb_w.pl
@@ -79,14 +79,9 @@ $result_converted{'html'}->{'spaces_in_line_break_in_verb_w'} = '<!DOCTYPE html>
 </html>
 ';
 
-$result_converted_errors{'html'}->{'spaces_in_line_break_in_verb_w'} = [
-  {
-    'error_line' => 'warning: must specify a title with a title command or @top
-',
-    'text' => 'must specify a title with a title command or @top',
-    'type' => 'warning'
-  }
-];
+$result_converted_errors{'html'}->{'spaces_in_line_break_in_verb_w'} = '* W |must specify a title with a title command or @top
+ warning: must specify a title with a title command or @top
 
+';
 
 1;
