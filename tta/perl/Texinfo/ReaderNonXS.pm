f22ee16740ed37488d30df85edf153f83b996bb2
Gavin Smith <gavinsmith0123@gmail.com>
2025-12-09 20:33:21 +0000

    tta/perl: use https: in licenses link

--- tta/perl/Texinfo/ReaderNonXS.pm
+++ tta/perl/Texinfo/ReaderNonXS.pm
@@ -13,7 +13,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 # The interface to the Texinfo tree provided in this file is similar to
 # Pod::Simple::PullParser or XML::LibXML::Reader
--- tta/perl/load_txi_modules.pl
+++ tta/perl/load_txi_modules.pl
@@ -15,7 +15,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 # Trimmed down texi2any.pl that is used to load the modules when
 # Perl code is directly called from C and no Perl script is run.
