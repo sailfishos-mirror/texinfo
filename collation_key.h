1f720013d98ba8c25e8d3c17a1ce9698e1c94289
Gavin Smith <gavinsmith0123@gmail.com>
2026-02-10 21:14:32 +0000

    split collation key algorithm into separate source file

---
+++ collation_key.h
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <stddef.h>
+
+typedef struct {
+  unsigned char *key;
+  size_t length;
+} CollationKey;
+
+CollationKey get_collation_key(CollationEntry *entries, uint32_t num_entries, uint32_t *codepoints, size_t length);
--- utf8_to_sort_key.c
+++ utf8_to_sort_key.c
@@ -5,6 +5,7 @@
 #include <getopt.h>
 
 #include "allkeys_bin_loader.h"
+#include "collation_key.h"
 
 
 /* UTF-8 validation and conversion result */
@@ -170,97 +171,9 @@ print_collation_key (UTF8Result result)
     if (!entries) {
         return 0;
     }
-
-    /* get array of collation entries */
-    CollationEntry **entry_array = malloc (sizeof (*entry_array)
-                                          * result.length);
-
-    /* only look for single codepoint collation entries */
-    CollationEntry *entry;
-
-    for (size_t i = 0; i < result.length; i++) {
-        entry = lookup_codepoint(entries, header.num_entries,
-                                 result.codepoints[i]);
-        if (entry) {
-            entry_array[i] = entry;
-        } else {
-            entry_array[i] = 0;
-            printf("Not found in database.\n");
-        }
-    }
-
-    int num_elements = 0;
-    for (size_t i = 0; i < result.length; i++) {
-      if (entry_array[i])
-        num_elements += entry_array[i]->num_elements;
-    }
-
-    unsigned char *sort_key;
-    unsigned char *psort_key;
-    size_t sort_key_alloc;
-
-    /* Three levels (primary/secondary/tertiary).  Two bytes per
-       collation element at first two levels, one byte at tertiary.
-       "\x00\x00" between levels and one final null. */
-    sort_key_alloc = num_elements * 5 + 4 + 1;
-
-    psort_key = sort_key;
-
-    sort_key = calloc (sort_key_alloc, 1);
-    psort_key = sort_key;
-
-    /* Primary */
-    for (size_t i = 0; i < result.length; i++) {
-      if (!entry_array[i])
-        continue;
-      for (size_t j = 0; j < entry_array[i]->num_elements; j++)
-        {
-          uint16_t weight = entry_array[i]->elements[j].primary;
-          if (weight)
-            {
-              *psort_key++ = weight >> 8;   /* More significant byte.*/
-              *psort_key++ = weight & 0xFF; /* Less significant byte.*/
-            }
-        }
-    }
-
-    *psort_key++ = '\x00';
-    *psort_key++ = '\x00';
-
-    /* Secondary */
-    for (size_t i = 0; i < result.length; i++) {
-      if (!entry_array[i])
-        continue;
-      for (size_t j = 0; j < entry_array[i]->num_elements; j++)
-        {
-          uint16_t weight = entry_array[i]->elements[j].secondary;
-          if (weight)
-            {
-              *psort_key++ = weight >> 8;   /* More significant byte.*/
-              *psort_key++ = weight & 0xFF; /* Less significant byte.*/
-            }
-        }
-    }
-
-    *psort_key++ = '\x00';
-    *psort_key++ = '\x00';
-
-    /* Tertiary */
-    for (size_t i = 0; i < result.length; i++) {
-      if (!entry_array[i])
-        continue;
-      for (size_t j = 0; j < entry_array[i]->num_elements; j++)
-        {
-          uint8_t weight = entry_array[i]->elements[j].tertiary;
-          if (weight)
-            {
-              *psort_key++ = weight;
-            }
-        }
-    }
-
+    CollationKey sort_key = get_collation_key(entries, header.num_entries, result.codepoints, result.length);
     printf ("Sort key: ");
-    for (unsigned char *p = sort_key; p < psort_key; p += 2)
+    for (unsigned char *p = sort_key.key; p < sort_key.key + sort_key.length; p += 2)
       {
         printf ("%02x%02x ", p[0], p[1]);
       }
