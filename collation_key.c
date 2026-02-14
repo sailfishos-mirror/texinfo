a177ce3bdc4cf33719a32fcbefd502ee70b3bd87
Gavin Smith <gavinsmith0123@gmail.com>
2026-02-14 12:21:28 +0000

    implicitly determine weights for Han ideographs

--- collation_key.c
+++ collation_key.c
@@ -3,10 +3,36 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include "unictype.h"
 #include "uninorm.h"
 
 #include "allkeys_bin_loader.h"
 #include "collation_key.h"
+
+void get_implicit_weight (uint32_t codepoint,
+                          CollationElement *elements,
+                          uint8_t *n_elements) {
+    uint32_t AAAA = 0, BBBB = 0;
+
+    if (uc_is_property_unified_ideograph (codepoint)) {
+        const uc_block_t *b = uc_block (codepoint);
+        if (   b->start == 0x4E00 /* CJK Unified Ideographs */
+            || b->start == 0xF900 /* CJK Compatibility Ideographs */)
+        {
+            AAAA = 0xFB40 + (codepoint >> 15);
+        } else {
+            AAAA = 0xFB80 + (codepoint >> 15);
+        }
+        BBBB = (codepoint & 0x7FFF) | 0x8000;
+    }
+    if (AAAA && BBBB) {
+      CollationElement e1 = {AAAA, 0x0020, 0x0002};
+      CollationElement e2 = {BBBB, 0x0000, 0x0000};
+      elements[0] = e1;
+      elements[1] = e2;
+      (*n_elements) = 2;
+    }
+}
 
 CollationKey get_collation_key(uint32_t *codepoints_in, size_t length_in) {
     uint32_t *codepoints;
@@ -21,7 +47,7 @@ CollationKey get_collation_key(uint32_t *codepoints_in, size_t length_in) {
 
     for (size_t i = 0; i < length; i++) {
         /* currently we only look for single codepoint collation entries */
-        uint32_t data_offset = lookup_codepoint_data(codepoints[i]);
+        CODEPOINT_DATA data_offset = lookup_codepoint_data(codepoints[i]);
         if (data_offset) {
             entry_array[i] = data_offset;
         } else {
@@ -34,6 +60,8 @@ CollationKey get_collation_key(uint32_t *codepoints_in, size_t length_in) {
     for (size_t i = 0; i < length; i++) {
       if (entry_array[i])
         num_elements += element_count_of_data_offset(entry_array[i]);
+      else
+        num_elements += 2; /* implicitly determined weights? */
     }
 
     CollationElement *elements = calloc (num_elements, sizeof (*elements));
@@ -44,6 +72,12 @@ CollationKey get_collation_key(uint32_t *codepoints_in, size_t length_in) {
             read_collation_data_offset(entry_array[i],
                                        &elements[elements_count],
                                        &num_entry_elements);
+            elements_count += num_entry_elements;
+        } else {
+            uint8_t num_entry_elements;
+            get_implicit_weight(codepoints[i],
+                                &elements[elements_count],
+                                &num_entry_elements);
             elements_count += num_entry_elements;
         }
     }
