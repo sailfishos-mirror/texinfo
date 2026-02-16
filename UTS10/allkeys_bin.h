07dfa133c784f5fa01edc55ba37fe90a7228703f
Gavin Smith <gavinsmith0123@gmail.com>
2026-02-16 17:37:33 +0000

    * info/terminal.c: use modern prototypes for curses
    This fixes compile errors with -std=gnu23 (which is the default
    with gcc 15) on systems that do not have termcap.h.

---
+++ UTS10/allkeys_bin.h
@@ -0,0 +1,81 @@
+#ifndef ALLKEYS_BIN_H
+#define ALLKEYS_BIN_H
+
+#include <config.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <uchar.h>
+
+/* Define data format for binary file. */
+
+/*
+ * Binary Format Specification:
+ * 
+ * Header (28 bytes):
+ *   char magic[8]        "UCADATA1"
+ *   uint32_t version     UCA version (e.g., 170000)
+ *   uint16_t max_variable_weight Maximum primary weight for Variable elements
+ *   uint32_t num_singles Number of single codepoint entries
+ *   uint32_t num_sequences Number of multi-codepoint sequences
+ *   uint32_t page_table_offset Offset to page table
+ *   uint32_t trie_offset Offset to sequence trie
+ * 
+ * Page Table (NUM_PAGES * 4 bytes):
+ *   For each page (4352 pages):
+ *     uint32_t page_offset  Offset to page data (0 if not allocated)
+ * 
+ * Page Data (variable size per page):
+ *   uint16_t count        Number of entries in page
+ *   For each entry:
+ *     uint8_t offset      Offset within page (0-255)
+ *     uint32_t data_offset Offset to collation data
+ * 
+ * Collation Data (variable size):
+ *   uint8_t num_elements
+ *   For each element:
+ *     uint16_t primary
+ *     uint8_t secondary (coded)
+ *     uint8_t tertiary
+ * 
+ * Sequence Trie (variable size):
+ *   uint32_t codepoint
+ *   uint32_t data_offset  (0 if intermediate node)
+ *   uint16_t num_children
+ *   For each child:
+ *     uint32_t child_offset
+ */
+
+
+/* Maximum codepoints in a sequence (observed max is 18, using 32 for safety) */
+#define MAX_SEQUENCE_LENGTH 32
+
+#define NUM_PAGES 4352
+
+#define MAX_COLLATION_ELEMENTS 18
+
+/* Binary file header */
+typedef struct
+{
+  char magic[8];
+  uint32_t version;
+  uint16_t max_variable_weight;
+  uint32_t num_singles;
+  uint32_t num_sequences;
+  uint32_t page_table_offset;
+  uint32_t trie_offset;
+} Header;
+
+/* Collation element - represents [.XXXX.XXXX.XXXX] or [*XXXX.XXXX.XXXX]. */
+typedef struct
+{
+  uint16_t primary;
+  uint16_t secondary;
+  uint8_t tertiary;
+} CollationElement;
+
+
+
+
+
+
+#endif /* ALLKEYS_BIN_H */
---
+++ UTS10/allkeys_query.c
@@ -0,0 +1,118 @@
+#include <config.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "allkeys_bin.h"
+#include "allkeys_bin_loader.h"
+
+void
+print_usage (const char *prog)
+{
+  printf ("Usage: %s <command> <binary_file> [options]\n\n", prog);
+  printf ("Commands:\n");
+  //printf("  info                     Show database information\n");
+  //printf("  dump <n>                 Dump first n entries\n");
+  //printf("  sequences                Dump all multi-codepoint sequences\n");
+  printf
+    ("  lookup <codepoint>...    Lookup codepoint(s) (hex, e.g., 0041 or 006C 00B7)\n\n");
+  printf ("Examples:\n");
+  //printf("  %s info allkeys.bin\n", prog);
+  //printf("  %s dump allkeys.bin 20\n", prog);
+  //printf("  %s sequences allkeys.bin\n", prog);
+  printf ("  %s lookup allkeys.bin 0041\n", prog);
+  printf ("  %s lookup allkeys.bin 006C 00B7\n", prog);
+}
+
+int
+main (int argc, char *argv[])
+{
+  if (argc < 3)
+    {
+      print_usage (argv[0]);
+      return 1;
+    }
+
+  const char *command = argv[1];
+  const char *binary_file = argv[2];
+
+  printf ("Loading collation data from %s...\n", binary_file);
+  if (!load_data_file (binary_file))
+    {
+      fprintf (stderr, "Failed to load data file\n");
+      return 1;
+    }
+  print_header_info ();
+
+#if 0
+  if (strcmp (command, "info") == 0)
+    {
+      dump_info (binary_file);
+    }
+  else if (strcmp (command, "dump") == 0)
+    {
+      int count = argc > 3 ? atoi (argv[3]) : 10;
+      dump_entries (binary_file, count);
+    }
+  else if (strcmp (command, "sequences") == 0)
+    {
+      dump_sequences (binary_file);
+    }
+  else
+#endif
+  if (strcmp (command, "lookup") == 0)
+    {
+      if (argc < 4)
+        {
+          fprintf (stderr, "Error: lookup requires at least one codepoint\n");
+          return 1;
+        }
+
+      char32_t codepoints[MAX_SEQUENCE_LENGTH];
+      size_t len = 0;
+
+      for (int i = 3; i < argc && len < MAX_SEQUENCE_LENGTH; i++)
+        {
+          unsigned long val = strtoul (argv[i], NULL, 16);
+          if (val > 0x10FFFF)
+            {
+              fprintf (stderr, "Error: invalid codepoint %s\n", argv[i]);
+              return 1;
+            }
+          codepoints[len++] = (char32_t) val;
+        }
+
+      /* Lookup in binary data format */
+      CollationElement elements[MAX_COLLATION_ELEMENTS];
+      size_t num_elements;
+
+      int found = 0;
+      if (len == 1)
+        {
+          found = lookup_codepoint (codepoints[0], elements, &num_elements);
+        }
+      else
+        {
+          found = lookup_sequence (codepoints, len, elements, &num_elements);
+        }
+
+      if (found)
+        {
+          print_collation (elements, num_elements);
+        }
+      else
+        {
+          printf ("NOT FOUND\n");
+        }
+    }
+  else
+    {
+      fprintf (stderr, "Unknown command: %s\n\n", command);
+      print_usage (argv[0]);
+      return 1;
+    }
+
+  return 0;
+}
